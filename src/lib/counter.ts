3f5979a8559213adac4765cf3f1eb891fad91495
Jonster5 <onlyaccelerating@gmail.com>
2022-05-18 16:18:31 +0000

    chin

--- src/lib/counter.ts
+++
@@ -1,9 +0,0 @@
-import { Writable } from 'svelte/store';
-
-export const increment = (c: Writable<number>) => {
-    c.update((n) => n + 1);
-};
-
-export const decrement = (c: Writable<number>) => {
-    c.update((n) => n - 1);
-};
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,43 +1,38 @@
 <script lang="ts">
-    import Counter from '@components/Counter.svelte';
 </script>
 
 <svelte:head>
-    <title>Home</title>
+  <title>Home</title>
 </svelte:head>
 
 <main>
-    <h1>Home</h1>
-    <h3>TCF Standard Svelte Template</h3>
-    <Counter />
+  <h1>Welcome to the Nav Party</h1>
 </main>
 
 <style lang="scss">
-    main {
-        display: flex;
+  main {
+    display: flex;
 
-        width: 100%;
-        height: 100%;
+    width: 100%;
+    height: 100%;
 
-        margin: 0;
-        padding: 0;
+    margin: 0;
+    padding: 0;
 
-        flex-direction: column;
-        align-items: center;
+    flex-direction: column;
+    align-items: center;
 
-        background: url('images/background.svg') no-repeat center center fixed;
-        background-size: cover;
+    background: url("images/background.svg") no-repeat center center fixed;
+    background-size: cover;
 
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 
-        h1 {
-            font-size: 4vw;
+    h1 {
+      font-size: 4vw;
 
-            color: white;
-        }
-
-        h3 {
-            color: rgb(35, 35, 35);
-        }
+      color: white;
     }
+
+    
+  }
 </style>
